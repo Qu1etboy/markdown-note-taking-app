0bd5815aa50ba83755566e15209ec093171616d7
Qu1etboy <nonzagreanthai@gmail.com>
2022-12-08 07:28:47 +0700

    Store firebase key in dotenv

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -8,17 +8,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAbCxE2qQTgB1ZZ_YJEsHvkm8_nM5tHqsk",
-  authDomain: "note-taking-app-62ff1.firebaseapp.com",
-  projectId: "note-taking-app-62ff1",
-  storageBucket: "note-taking-app-62ff1.appspot.com",
-  messagingSenderId: "147311856151",
-  appId: "1:147311856151:web:a41d6908910461e9b2ea52",
-  measurementId: "G-TXPV9D47LP",
-};
 
-// Initialize Firebase
+// console.log(import.meta.env.VITE_API_KEY);
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth();
